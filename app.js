eb3ddffed7ea64443b58565ac4d8d7995708bdb7
Verónica Natalia Correa <99627791+veru85@users.noreply.github.com>
2022-12-28 20:01:02 +0100

    Subiendo portfolio

---
+++ app.js
@@ -0,0 +1,14 @@
+let number1 = "7"
+let number2 = "4"
+let number3 = "2"
+
+let concat = number1 + number2 + number3
+
+let convertir = Number(number1)+Number(number2)+ Number(number3)
+
+console.log(concat);
+console.log(convertir);
+
+console.log( "el resultado es: "+ convertir);
+
+console.log(typeof(toString(convertir)));
